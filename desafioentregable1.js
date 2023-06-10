c877261041abba6c4a134ecc817da4881c07a138
santiago maza <mazasantiago.10@gmail.com>
2023-06-10 15:17:59 -0300

    add products resuelto

--- desafioentregable1.js
+++ desafioentregable1.js
@@ -1,108 +1,117 @@
-const fs = require('fs')
+const fs = require("fs");
 
 class ProductManager {
+  constructor(path) {
+    this.products = [];
+    this.path = path;
+  }
 
-    constructor (path) {
-        this.products = [];
-        this.path = path
+  #id = 0;
+
+  #requiredFields(product) {
+    if (
+      !!product.title &&
+      !!product.description &&
+      !!product.price &&
+      !!product.code &&
+      !!product.thumbnail &&
+      !!product.stock
+    ) {
+      return true;
     }
+  }
 
-    
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const productsjson = await fs.promises.readFile(this.path, "utf-8");
+        const products = JSON.parse(productsjson);
+        return (this.products = products);
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    #id = 0;
-    
-    async getProducts(){
+  async addProduct(product) {
+    if (this.#requiredFields(product)) {
+      if (
+        !this.products.some(
+          (p) => p.code === product.code || this.products.length == 0
+        )
+      ) {
+        this.#id++;
         try {
-            if (fs.existsSync(this.path)) {
-                const productsjson = await fs.promises.readFile(this.path, 'utf-8')
-                const products = JSON.parse(productsjson)
-                return products
-            } else {
-                return []
-            }
+          const productsFile = await this.getProducts();
+          const productwid = { ...product, id: this.#id };
 
+          productsFile.push(productwid);
+          await fs.promises.writeFile(this.path, JSON.stringify(productsFile));
         } catch (error) {
-            console.log(error)
+          console.log(error);
         }
-        
+      } else {
+        console.log("El producto ya existe");
+      }
+    } else {
+      console.log("Todos los campos son obligatorios, por favor completar");
     }
+  }
 
-    async addProduct(product){
-
-        if (( !!product.title &&
-            !!product.description &&
-            !!product.price &&
-            !!product.code &&
-            !!product.thumbnail && 
-            !!product.stock )) {
-            
-                if ( this.products.some( (p) => p.code !== product.code)) {
-                    try {
-                        const productsFile = await this.getProducts();
-                        this.#id++
-                        const productwid = {...product, id: this.#id}
-    
-                        productsFile.push(productwid)
-                        await fs.promises.writeFile(this.path, JSON.stringify(productsFile))
-                    } catch (error) {
-                        console.log(error)
-                    }
-                } else {
-                    console.log("El producto ya existe")
-                }
-                    
-        } else {
-            console.log("Todos los campos son obligatorios, por favor completar")
-        }        
+  getProductById = (productId) => {
+    const prod = this.products.find((product) => product.id === productId);
+    if (prod) {
+      return prod;
+    } else {
+      console.log("Product not found");
+      return "Product not found";
     }
+  };
 
-
-
-    getProductById = (productId) => {
-        const prod = this.products.find(product => product.id === productId)
-        if (prod) {
-            console.log(prod)
-            return prod
-        } else {
-            console.log("Product not found")
-            return "Product not found"
-    }
-}
 }
 
-
-const ejemplo = new ProductManager('./products.json')
+const manager = new ProductManager("./products.json");
 
 const product1 = {
-    title : "producto de prueba",
-    description: "Este es un producto de prueba",
-    price: 200,
-    code: "abc123",
-    thumbnail: "Sin imagen",
-    stock: 25
-}
+  title: "producto de prueba",
+  description: "Este es un producto de prueba",
+  price: 200,
+  code: "abc123",
+  thumbnail: "Sin imagen",
+  stock: 25,
+};
 
 const product2 = {
-    title : "producto de prueba 2",
-    description: "Este es otro producto de prueba",
-    price: 500,
-    thumbnail: "Sin imagen",
-    code: "abc1234567789",
-    stock: 50
-}
+  title: "producto de prueba 2",
+  description: "Este es otro producto de prueba",
+  price: 500,
+  thumbnail: "Sin imagen",
+  code: "abc1234567789",
+  stock: 50,
+};
 
 const test = async () => {
-    const getProducts = await ejemplo.getProducts()
-    console.log('Primer consulta sin productos agregados => ',getProducts)
-    await ejemplo.addProduct(product1)
-    const getProductos2 = await ejemplo.getProducts()
-    console.log('2da consulta =>', getProductos2)
-    await ejemplo.addProduct(product1)
-    const getProductos3 = await ejemplo.getProducts()
-    console.log('3er consulta =>', getProductos3)
-}
+  const getProducts = await manager.getProducts();
+  console.log("Primer consulta sin productos agregados => ", getProducts);
 
-test()
+      await manager.addProduct(product1) //AGREGANDO PRODUCT1
+    console.log("Producto agregado")
+
+    const getProductos2 = await manager.getProducts()
+    console.log('2da consulta con un producto agregado =>', getProductos2)
+
+    await manager.addProduct(product2) //AGREGANDO PRODUCT2
+    console.log("2do producto agregado")
+
+    const getProductos3 = await manager.getProducts()
+    console.log('3er consulta con 2 productos agregados =>', getProductos3)
+
+};
+
+test();
+
 /* const ejemplo = new ProductManager; // Instancia creada
 
 console.log("PRUEBA DE ARRAY VACIO",ejemplo.getProducts()) // Array vacio
